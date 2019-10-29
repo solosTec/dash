eb94fd2218cf116e3fa0b1c2cf6c907d8ce657df
Michael Seemann <michael.seemann@synaos.com>
2019-10-29 20:25:20 +0100

    remove eslint warnings during build

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,10 @@
 module.exports = {
+    extends: [
+        // add more generic rulesets here, such as:
+        // 'eslint:recommended',
+        'plugin:vue/recommended'
+    ],
     rules: {
         'no-console': 'off',
-    },
+    }
 };
