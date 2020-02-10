2f814be192b8cdab536dfd77323a46481c83ca34
Michael Seemann <michael.seemann@synaos.com>
2020-02-10 16:42:53 +0100

    backend api

---
+++ src/backend-api/user.ts
@@ -0,0 +1,12 @@
+export enum UserRole {
+    SystemAdmin ="SystemAdmin",
+    Viewer ="Viewer",
+    DeviceManager = "DeviceManager"
+}
+
+export interface User {
+    username: string;
+    passwort: string;
+    role: UserRole;
+    lastAccess: string; // UTC ISO Date "2019-12-11 08:57:14.00000000"
+}
