0baf6444e20128c60b0743a7339bde82f977a083
Michael Seemann <michael.seemann@synaos.com>
2020-05-16 17:30:58 +0200

    build smf and smf-dev

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 // vue.config.js
 module.exports = {
     publicPath: process.env.VUE_APP_SMF_DOCKER === 'true'
-        ? '/smf/'
+        ? '/'+process.env.VUE_APP_SMF_PUBLIC_PATH+'/'
         : '/'
 };
